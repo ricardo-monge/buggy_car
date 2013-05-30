30eef349c744d160e38a7a406da28a55f352f169
ricardo-monge <ricardo.monge@naitcorp.com>
2013-05-30 17:53:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuggyCar::Application.config.secret_token = 'ea6ababda1e347cb315cd095af5641c27a1cdbfdb2f6b61a9a7b6c5219035d85370b3c8aa8b7e2a1fcccbfe721874941b6d02dd705443db4103ac8fe51d087c5'
